38ab3a4a544d53937785f4511a9231661b41c22d
abhix6 <abhi.69km@gmail.com>
2025-01-23 21:47:40 +0530

    Update OurAchievement.tsx

--- src/pages/OurAchievement.tsx
+++ src/pages/OurAchievement.tsx
@@ -4,53 +4,114 @@ function OurAchievement() {
   // Replace these URLs with your actual achievement photos
   const achievements = [
     {
-      image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/boss.jpg',
-      title: 'Award of excellence'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1523287562758-66c7fc58967f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Excellence in Customer Service'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1531545514256-b1400bc00f31?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Industry Leadership Award'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1588196749597-9ff075ee6b5b?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Top Insurance Broker 2022'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Customer Satisfaction Award'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1567427017947-545c5f8d16ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Digital Innovation Award'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1553729459-efe14ef6055d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Best Insurance Solutions 2023'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Service Excellence Certificate'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1607344645866-009c320b63e0?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Insurance Leader Award'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1624953587687-daf255b6b80a?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Best Customer Experience'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Innovation Excellence'
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1562654501-a0ccc0fc3fb1?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      title: 'Outstanding Performance 2023'
-    }
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/1.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/2.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/3.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/4.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/5.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/6.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/7.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/8.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/9.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/10.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/11.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/12.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/13.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/14.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/15.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/16.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/17.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/18.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/19.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/20.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/21.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/22.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/23.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/24.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/25.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/26.jpg',
+  title: ''
+},
+{
+  image: 'https://raw.githubusercontent.com/abhix6/PolicyKendra/main/images/27.jpg',
+  title: ''
+}
+
   ];
 
   return (
